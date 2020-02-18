4e1725d28884e894d1c549e44594c4f7780893d4
qwe <spencer@grr.la>
2020-02-18 13:13:19 +0200

    commit

--- src/main/HomeWork.java
+++ src/main/HomeWork.java
@@ -1,11 +1,12 @@
 package main;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class HomeWork {
 
-    public int multiplication(int... mnogiteli) throws IllegalArgumentException {
+    public static int multiplication(int... mnogiteli) throws IllegalArgumentException {
         // Если не было введено ни одного параметра, выбрасывается исключение
         if (mnogiteli.length == 0) {
             throw new IllegalArgumentException("invalid parameters length");
@@ -14,25 +15,46 @@ public class HomeWork {
         }
 
         int result[] = {1};
-        Arrays.stream(mnogiteli).forEach(i -> {
-            result[0] *= i;
-        });
+        Arrays.stream(mnogiteli).forEach(i -> result[0] *= i);
         return result[0];
     }
 
-
-    public static <T extends Comparable> List<T> bubbleSortArrayList(List<T> inputList) {
+    public <T extends Comparable<? super T>> List<T> bubbleSortArrayList(List<T> inputList) {
+        if (inputList.size() <= 1) {
+            return inputList;
+        }
         int n = inputList.size();
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
-                if ((inputList.get(j).compareTo(inputList.get(j + 1))) > 0) {
-                    T temp = inputList.get(j);
-                    inputList.set(j, inputList.get(j + 1));
-                    inputList.set(j + 1, temp);
+                if (inputList.get(j).compareTo(inputList.get(j + 1)) > 0) {
+                    swap(inputList, j, j+1);
                 }
             }
         }
         return inputList;
+    }
+
+    public <T extends Comparable<? super T>> List<T> selectionSort(List<T> list){
+        for (int lastUnsortedIndex = list.size() - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            int largest = 0;
+            for (int i = 1; i <= lastUnsortedIndex; i++) {
+                if (list.get(i).compareTo(list.get(largest)) > 0) {
+                    largest = i;
+                }
+            }
+            swap(list, largest, lastUnsortedIndex);
+        }
+        return list;
+    }
+
+    public static <T extends Comparable<? super T>> void swap(List<T> list, int position1, int position2) {
+        if (list.get(position1).equals(list.get(position2))) {
+            return;
+        }
+        T temp = list.get(position2);
+        list.set(position2, list.get(position1));
+        list.set(position1, temp);
+
     }
 
 }
--- src/test/HomeWorkTest.java
+++ src/test/HomeWorkTest.java
@@ -2,20 +2,21 @@ package test;
 
 import main.HomeWork;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HomeWorkTest {
     public static HomeWork homeWork;
@@ -31,25 +32,24 @@ class HomeWorkTest {
 
 
     @ParameterizedTest
-    @MethodSource("argumentsSourceForAdditionTest")
-    public <T extends Comparable> void bubbleSortTestShouldReturnSortedArrayListofComparables(List<T> inputList, List<T> expected) {
+    @MethodSource("streamOfArgumentsArrayListFromFiles")
+    public <T extends Comparable<? super T>> void bubbleSortTestShouldReturnSortedArrayListofComparables(
+            List<T> inputList, List<T> expected) {
         List<T> result = homeWork.bubbleSortArrayList(inputList);
         assertEquals(expected, result);
     }
 
-//    @Test
-//    public void bubbleSortTestShouldThrowIllegalArgumentException(){
-//        assertThrows(IllegalArgumentException.class, ()->homeWork.bubbleSortArrayList(new));
-//    }
+    @ParameterizedTest
+    @MethodSource("streamOfArgumentsArrayListFromFiles")
+    public void selectionSortTestShouldReturnSortedList(List<String> inputList, List<String> expected) {
+        List<String> result = homeWork.selectionSort(inputList);
+        assertEquals(expected, result);
+    }
 
-
-    static Stream<Arguments> argumentsSourceForAdditionTest() throws FileNotFoundException {
-        List<String> list = getFileAsList("src/unsortedStrings");
-        List<String> sotredList = new ArrayList<>(list);
-        sotredList.sort(String::compareTo);
-
+    static Stream<Arguments> argumentsSourceForSortTest() {
         return Stream.of(
-                Arguments.arguments(list, sotredList),
+                Arguments.arguments(getFileAsList("src/unsortedStrings"),
+                        getFileAsList("src/unsortedStrings").stream().sorted(String::compareTo).collect(Collectors.toList())),
                 Arguments.arguments(Arrays.asList(""), Arrays.asList("")),
                 Arguments.arguments(Arrays.asList("asdf", "ssfg", "", "zzzasdf", "qwerqw", "asdfgsa"), Arrays.asList("", "asdf", "asdfgsa", "qwerqw", "ssfg", "zzzasdf")),
                 Arguments.arguments(Arrays.asList("asdf", "ssfg", "zzzasdf", "0", "qwerqw", "asdfgsa"), Arrays.asList("0", "asdf", "asdfgsa", "qwerqw", "ssfg", "zzzasdf")),
@@ -57,12 +57,24 @@ class HomeWorkTest {
         );
     }
 
+    static Stream<Arguments> streamOfArgumentsArrayListFromFiles() {
+        List<Arguments> argList = new ArrayList<>();
+
+        // небольшой эксперимент с внешними ресурсами для тестирования
+        // текстовый файл "src/listOfTextFiles", содержит в себе список файлов для тестирования,
+        // добавляем содержимого каждого файла из списка в виде
+        // List<String> в переменную argList  и затем возвращаем ее в виде потока
+        getFileAsList("src/listOfTextFiles").forEach(s -> {
+            argList.add(Arguments.arguments(getFileAsList(s), getFileAsList(s).parallelStream().sorted(String::compareTo).collect(Collectors.toList())));
+        });
+
+        return argList.stream();
+    }
+
     public static List<String> getFileAsList(String fileUri) {
         List<String> result = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(new File(fileUri)))) {
             result = reader.lines().collect(Collectors.toList());
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
