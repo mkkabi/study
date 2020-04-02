ca309ea70d7834175e69b721ac4f0806d0a56f90
mkkabi <mkkabi@grr.la>
2020-04-02 09:52:46 -0400

    comm

---
+++ src/main/MyArrayList.java
@@ -0,0 +1,27 @@
+package main;
+
+import java.util.AbstractCollection;
+import java.util.Collection;
+import java.util.Iterator;
+
+public  class MyArrayList<T> extends AbstractCollection<T> {
+    private final int size;
+    T[] arr;
+
+    public MyArrayList( Collection<T> collection) {
+        this.size = collection.size();
+        arr = new [size];
+    }
+
+
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
---
+++ src/main/experiment/User.java
@@ -0,0 +1,7 @@
+package main.experiment;
+
+public class User {
+    public User() {
+        System.out.println("Work constructor User");
+        two();
+    }
